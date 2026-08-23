5eb183da3bd3feb9f98f409db66c5b715feb9993
YashasVM <admin5@duck.com>
2026-08-24 02:24:48 +0530

    fix(windows): start when windir is not inherited

--- windows/src/CD.Windows/App.xaml.cs
+++ windows/src/CD.Windows/App.xaml.cs
@@ -4,6 +4,12 @@ namespace CD.Windows;
 
 public partial class App : System.Windows.Application
 {
+    public App()
+    {
+        if (string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("windir")))
+            Environment.SetEnvironmentVariable("windir", Environment.GetFolderPath(Environment.SpecialFolder.Windows));
+    }
+
     protected override void OnSessionEnding(SessionEndingCancelEventArgs e)
     {
         (MainWindow as CD.Windows.MainWindow)?.PrepareForSystemShutdown();
