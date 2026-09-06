a6cb6674fb0a06ec92f7825b6a70c25d27152932
YashasVM <admin5@duck.com>
2026-09-06 10:25:51 +0530

    chore: reset repository

--- cdx/internal/cli/layout.go
+++
@@ -1,9 +0,0 @@
-package cli
-
-// These layout identifiers preserve the help customization fields used by the
-// parser while keeping cdx's renderer independent of text/template.
-var (
-	AppHelpTemplate        = "app"
-	CommandHelpTemplate    = "command"
-	SubcommandHelpTemplate = "subcommand"
-)
--- cdx/src/version/version.go
+++
@@ -1,5 +0,0 @@
-// Package version contains the version shared by the cdx executables.
-package version
-
-// Value is the current cdx release version.
-const Value = "11.2.5"
