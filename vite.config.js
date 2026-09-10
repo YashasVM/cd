cf698f25aa0c3ce4b0a90300a2b0ea2876f970f2
YashasVM <admin5@duck.com>
2026-09-10 21:50:27 +0530

    Make legacy web build the sole project

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig, loadEnv } from 'vite';
-
-export default defineConfig(({ mode }) => {
-  const clerk = loadEnv(mode, process.cwd(), 'CLERK_PUBLISHABLE_KEY');
-  const vite = loadEnv(mode, process.cwd(), 'VITE_CLERK_PUBLISHABLE_KEY');
-  return { define: { 'import.meta.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(vite.VITE_CLERK_PUBLISHABLE_KEY || clerk.CLERK_PUBLISHABLE_KEY) } };
-});
